dde89908cedc79c292a02e6c5c0633d03171cc29
reyalexander <alexandercayromamani@gmail.com>
2023-03-16 17:24:24 -0500

    FEAT: add user's CRUD with JWT authentication

--- user/views.py
+++ user/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from .serializer import UserSerializer
+from rest_framework import viewsets
+from django.contrib.auth.hashers import make_password
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def perform_create(self, serializer):
+        return serializer.save(password = make_password(serializer.validated_data['password']))
