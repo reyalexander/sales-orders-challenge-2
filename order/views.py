0418c0ade9a438e02918172abdc8227a3487a483
reyalexander <alexandercayromamani@gmail.com>
2023-03-18 13:05:36 -0500

    CHORE: Documenting the APIs using Swagger

--- order/views.py
+++ order/views.py
@@ -1,7 +1,9 @@
 from .models import Order
 from .serializer import OrderSerializer
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 
 class OrderViewset(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
--- sales/urls.py
+++ sales/urls.py
@@ -14,11 +14,29 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Order Sales API",
+        default_version="v1",
+        description="API to create order's CRUD aplication DJANGO",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="alexandercayromamani@gmail.com"),
+        license=openapi.License(name="MIT")
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny]
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('user/', include('user.urls')),
-    path('order/', include('order.urls'))
+    path('order/', include('order.urls')),
+    path('swagger/', schema_view.with_ui("swagger", cache_timeout=0), name="swagger-docs"),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
